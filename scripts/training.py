30b46467d149fab0c6e00e9a372ef44efa8b9eda
Vito Vernellati <vito.vernellati03@gmail.com>
2025-01-31 12:35:57 +0100

    mod: Aggiunta link Colab

---
+++ scripts/training.py
@@ -0,0 +1,18 @@
+# Obiettivo:
+
+# Caricare le immagini e applicare data augmentation
+# Usare Transfer Learning (EfficientNetB0)
+# Salvare il miglior modello
+# Visualizzare le performance (Accuracy/Loss)
+
+# Carica il dataset e applica data augmentation
+# Usa Transfer Learning (EfficientNetB0)
+# Blocca i pesi pre-addestrati e aggiunge nuovi layer
+# Compila il modello con Adam e Binary Crossentropy
+# Usa ModelCheckpoint per salvare il miglior modello
+# Usa EarlyStopping per fermare l'addestramento in caso di overfitting
+# Salva la cronologia in training_history.json
+# Genera un grafico delle performance (training_plot.png)
+
+# GOOGLE COLAB
+# https://colab.research.google.com/drive/1Ru5fmUcEQXs09vkkfYFBwTmVpVQiMzoG
